0943b650bbd0869fbd0be7ed3c4d1319feacc2cb
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2025-09-26 06:27:22 +0200

    created gradio client app

---
+++ unit-02/mcp-sentiment/gradio-app.py
@@ -0,0 +1,26 @@
+import gradio as gr
+import os
+
+from smolagents import InferenceClientModel, CodeAgent, MCPClient
+
+
+try:
+    mcp_client = MCPClient(
+        {"url": "https://abidlabs-mcp-tool-http.hf.space/gradio_api/mcp/sse", "transport": "sse",}
+    )
+    tools = mcp_client.get_tools()
+
+    model = InferenceClientModel(token=os.getenv("HUGGINGFACE_API_TOKEN"))
+    agent = CodeAgent(tools=[*tools], model=model, additional_authorized_imports=["json", "ast", "urllib", "base64"])
+
+    demo = gr.ChatInterface(
+        fn=lambda message, history: str(agent.run(message)),
+        type="messages",
+        examples=["Analyze the sentiment of the following text 'This is awesome'"],
+        title="Agent with MCP Tools",
+        description="This is a simple agent that uses MCP tools to answer questions.",
+    )
+
+    demo.launch()
+finally:
+    mcp_client.disconnect()
